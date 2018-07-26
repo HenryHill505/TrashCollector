6dce6a28c5a3d557a55d8c023586bd6ea3ff1d14
HenryHill505 <hhill505@gmail.com>
2018-07-26 15:28:07 -0500

    Added roles to Users table

--- TrashCollector/Models/IdentityModels.cs
+++ TrashCollector/Models/IdentityModels.cs
@@ -17,6 +17,7 @@ namespace TrashCollector.Models
         public string State { get; set; }
         public string PickupDay { get; set; }
         public string PickupRate { get; set; }
+        public string UserRole { get; set; }
 
 
         public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
