97b921b9b33d82e6a891679008c179242239dd21
HenryHill505 <hhill505@gmail.com>
2018-07-26 15:59:02 -0500

    Replaced the View() returned in AcountController.Register with a RedirectToAction()

--- TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/Controllers/AccountController.cs
@@ -165,8 +165,7 @@ namespace TrashCollector.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    //return RedirectToAction("Index", "Home");
-                    return View("Index", "Home", user);
+                    return RedirectToAction("Index", "Home");
                 }
                 AddErrors(result);
             }
