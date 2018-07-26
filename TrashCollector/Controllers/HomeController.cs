6df61e8c17199aafb50405d37377bbb9375a62b9
HenryHill505 <hhill505@gmail.com>
2018-07-26 15:15:20 -0500

    Changed default route to go to login page. Modified HomeController.Index() to pass a user into the view if the the current user is authenticated. Modified Index view to present user info if the current user is authenticated"

--- TrashCollector/Controllers/HomeController.cs
+++ TrashCollector/Controllers/HomeController.cs
@@ -1,16 +1,29 @@
-﻿using System;
+﻿using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Security;
+using TrashCollector.Models;
 
 namespace TrashCollector.Controllers
 {
     public class HomeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
         public ActionResult Index()
         {
-            return View();
+            if (User.Identity.IsAuthenticated)
+            {
+                string userId = User.Identity.GetUserId();
+                var user = db.Users.Where(u => u.Id == userId).FirstOrDefault();
+                return View(user);
+            }
+            else
+            {
+                return View();
+            }
         }
 
         public ActionResult About()
