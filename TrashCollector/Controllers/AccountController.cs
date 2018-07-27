a2a978743107b257836ff2b2a501634e1deb6943
HenryHill505 <hhill505@gmail.com>
2018-07-27 07:39:50 -0500

    Made Register and Login methods in AccountController redirect to the Customer controller

--- TrashCollector/Controllers/AccountController.cs
+++ TrashCollector/Controllers/AccountController.cs
@@ -81,7 +81,8 @@ namespace TrashCollector.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    //return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Customer");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
@@ -165,7 +166,7 @@ namespace TrashCollector.Controllers
                     // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
                     // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
 
-                    return RedirectToAction("Index", "Home");
+                    return RedirectToAction("Index", "Customer");
                 }
                 AddErrors(result);
             }
