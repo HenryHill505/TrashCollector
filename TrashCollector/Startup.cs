b2876f285728025ebe7290e2a66c8774ab226b6e
HenryHill505 <hhill505@gmail.com>
2018-07-27 14:19:28 -0500

    Fixed bug in Startup.SchedulePickups()

--- TrashCollector/Startup.cs
+++ TrashCollector/Startup.cs
@@ -51,9 +51,9 @@ namespace TrashCollector
                 DateTime pickupDay = DateTime.Today;
                 while (pickupDay.DayOfWeek.ToString() != user.PickupDay && (pickupDay.DayOfWeek.ToString() != "Saturday" && pickupDay.DayOfWeek.ToString() != "Sunday"))
                 {
-                    pickupDay.AddDays(1);
+                    pickupDay = pickupDay.AddDays(1);
                 }
-                if (pickupDay.DayOfWeek.ToString() == user.PickupDay && pickups.Where(p => p.UserId == user.Id).Where(p => p.Date == pickupDay.Date) == null)
+                if (pickupDay.DayOfWeek.ToString() == user.PickupDay && pickups.Where(p => p.UserId == user.Id).Where(p => p.Date == pickupDay.Date).Count() == 0)
                 {
                     Pickup pickup = new Pickup() { UserId = user.Id, User = user, Date = pickupDay.Date, Cost = 1, Status = "Incomplete" };
                     db.Pickups.Add(pickup);
