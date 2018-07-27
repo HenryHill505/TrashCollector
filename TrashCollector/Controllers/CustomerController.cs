b4ac2cb85591d458f72bdec8df89fbb1e98b2293
HenryHill505 <hhill505@gmail.com>
2018-07-27 07:35:22 -0500

    Added CustomerController and the corresponding views

---
+++ TrashCollector/Controllers/CustomerController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TrashCollector.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ExtraPickup()
+        {
+            return View();
+        }
+
+        public ActionResult PaymentOwed()
+        {
+            return View();
+        }
+
+        public ActionResult PickupDay()
+        {
+            return View();
+        }
+
+        public ActionResult Suspension()
+        {
+            return View();
+        }
+    }
+}
