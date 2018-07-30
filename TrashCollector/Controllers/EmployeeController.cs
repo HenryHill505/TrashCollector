5edded44766ecb7da20710f91889ef1eaced5c01
HenryHill505 <hhill505@gmail.com>
2018-07-30 09:28:10 -0500

    Added logic to EmployeeController.Index()

--- TrashCollector/Controllers/EmployeeController.cs
+++ TrashCollector/Controllers/EmployeeController.cs
@@ -3,15 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TrashCollector.Models;
+using Microsoft.AspNet.Identity;
 
 namespace TrashCollector.Controllers
 {
     public class EmployeeController : Controller
     {
+        ApplicationDbContext db = new ApplicationDbContext();
+
         // GET: Employee
         public ActionResult Index()
         {
-            return View();
+            string id = User.Identity.GetUserId();
+            int zip = db.Users.Where(u => u.Id == id).Select(u => u.ZipCode).FirstOrDefault();
+            List<Pickup> pickups = db.Pickups.Where(p => p.User.ZipCode == zip).ToList();
+            return View(pickups);
         }
     }
 }
